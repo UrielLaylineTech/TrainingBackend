c09f5fe8c0b22b5cdafe694052d149c305af43f6
UrielLaylineTech <uriel@laylinetech.com>
2020-01-08 12:23:10 +0200

    added comments and design

--- currency_rate_live_boi/models/res_config_settings.py
+++ currency_rate_live_boi/models/res_config_settings.py
@@ -19,7 +19,7 @@ _logger = logging.getLogger(__name__)
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
-
+    # adding BOI to options of selecting the company's currency provider
     currency_provider = fields.Selection(selection_add=[('boi', 'Bank Of Israel')])
    
 
@@ -28,7 +28,7 @@ class ResCompany(models.Model):
         res = super(ResCompany, self).set_special_defaults_on_install()
         for company in self.env['res.company'].search([]):
             if company.country_id.code == 'IL':
-                #Bank of Israel
+                #Bank of Israel configured as currency provider of Israeli companies
                 company.currency_provider = 'boi'
         return res
 
@@ -41,8 +41,6 @@ class ResCompany(models.Model):
         request_url = "https://www.boi.org.il/currency.xml"
         try:
             parse_url = requests.request('GET', request_url)
-            print("connected to url boi")
-            print(parse_url.content)
         except:
             #connection error, the request wasn't successful
             return False
@@ -50,8 +48,6 @@ class ResCompany(models.Model):
         xmlstr = etree.fromstring(parse_url.content)
         data = xml2json_from_elementtree(xmlstr)
         rates_dict = {}
-        print("show in json")
-        print (data)
 
         available_currency_names = available_currencies.mapped('name')
 
@@ -60,12 +56,7 @@ class ResCompany(models.Model):
             if child_node['tag'] == 'CURRENCY':
                 for sub_child in child_node['children']:
                     if sub_child['tag'] == "CURRENCYCODE":
-                        print('got here child')
                         currency_code = sub_child['children'][0]
-                        print("curcode:")
-                        print(currency_code)
-                        print("available:")
-                        print(available_currency_names)
 
                         add_to_dict = False
 
@@ -77,14 +68,10 @@ class ResCompany(models.Model):
                         currency_xml = sub_child['children'][0]
                     elif sub_child['tag'] == 'RATE':
                         rate_xml = sub_child['children'][0]
-                    # if currency_xml and rate_xml:
-                    #     #avoid iterating for nothing on children
-                    #     break
+ 
                 if add_to_dict:
+                    #compute actual rate against ILS by dividing units by rate.
                     rates_dict[currency_code] = (float(currency_xml) / float(rate_xml), fields.Date.today())
-
-        print("rates:")
-        print(rates_dict)
 
         if 'ILS' in available_currency_names:
             rates_dict['ILS'] = (1.0, fields.Date.today())
